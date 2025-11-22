443d261e6d92caba8e163a2258225e22e570f75e
lebedev-023046 <lebedev.023046@gmail.com>
2025-11-22 15:25:32 +0300

    feat: configure prisma & init habit module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { HabitModule } from './habit/habit.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HabitModule],
   controllers: [AppController],
   providers: [AppService],
 })
