1124d3bc3915244f30bb46cb2f226d9009847b20
lebedev-023046 <lebedev.023046@gmail.com>
2025-11-23 02:12:37 +0300

    refactor: improve error-handling function

--- backend/src/common/helper/error-handling.ts
+++ backend/src/common/helper/error-handling.ts
@@ -3,7 +3,58 @@ interface getErrorMessageProps {
   errorMessage: string;
 }
 
+const getMissingFieldError = (error: any) => {
+  const missingFieldMatch = error.message.match(
+    /Argument \`(\w+)\` is missing/,
+  );
+  if (missingFieldMatch) {
+    const fieldName = missingFieldMatch[1];
+    return `The ${fieldName} field is required`;
+  }
+};
+
+const getInvalidTypeErrpr = (error: any) => {
+  const errorMessage = error?.message ?? error;
+  const patterns = [
+    {
+      pattern: /Argument \`(\w+)\` is missing/,
+      handler: (match: RegExpMatchArray) => `The ${match[1]} field is required`,
+    },
+    {
+      pattern: /Invalid value for argument \`(\w+)/,
+      handler: (match: RegExpMatchArray) =>
+        `Invalid format for ${match[1]}. Please use a valid format.`,
+    },
+    {
+      pattern: /Argument \`(\w+)\`: Invalid value/,
+      handler: (match: RegExpMatchArray) =>
+        `Invalid type for field: ${match[1]}`,
+    },
+    {
+      pattern: /input contains invalid characters/,
+      handler: () => 'Invalid data format provided',
+    },
+    {
+      pattern: /Unknown argument/,
+      handler: (match: RegExpMatchArray) => 'Unknown field provided in request',
+    },
+  ];
+
+  // Проверяем все паттерны
+  for (const { pattern, handler } of patterns) {
+    const match = errorMessage.match(pattern);
+    if (match) {
+      return handler(match);
+    }
+  }
+};
+
 export function throwError({ error, errorMessage }: getErrorMessageProps) {
+  const missingFieldErrorMessage = getMissingFieldError(error);
+  const invalidTypeErrorMessage = getInvalidTypeErrpr(error);
+
+  console.log({ invalidTypeErrorMessage });
+
   const errorMap: Record<string, string> = {
     P2002: 'This record already exists',
     P2025: 'The requested item was not found',
@@ -16,6 +67,10 @@ export function throwError({ error, errorMessage }: getErrorMessageProps) {
   };
 
   const validatedErrorMessage =
-    errorMap[error.code] || error.message || 'Something went wrong';
+    errorMap[error.code] ||
+    missingFieldErrorMessage ||
+    invalidTypeErrorMessage ||
+    error.message ||
+    'Something went wrong';
   throw new Error(`${errorMessage} ${validatedErrorMessage}`);
 }
