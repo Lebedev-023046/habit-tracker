e177ea2fd24476a23ce8735cdc57bfa42f60503c
lebedev-023046 <lebedev.023046@gmail.com>
2025-12-03 22:52:10 +0300

    feat: make kanban habit card draggable

---
+++ frontend/src/features/habit-kanban/ui/habit-kanban-card/HabitKanbanCard.tsx
@@ -0,0 +1,140 @@
+import type { UpdateHabitFormValues } from '@/entities/habit/model/form/schema';
+import type { HabitStatus, HabitTotalDays } from '@/entities/habit/model/types';
+import { DeleteHabitModalTrigger } from '@/features/habit/delete/ui/delete-habit-modal-trigger';
+import { UpdateHabitModalTrigger } from '@/features/habit/update/ui/update-habit-modal-trigger';
+import { UpdateHabitStatusButton } from '@/features/habit/update/ui/update-habit-status-button';
+import { useClickOutside } from '@/shared/hooks/useClickOutside';
+import { Button } from '@/shared/ui/button';
+import { DailyCalendarProgress } from '@/shared/ui/daily-calendar-progress';
+import { ProgressBar } from '@/shared/ui/progress-bar';
+import { Subtitle } from '@/shared/ui/subtitle';
+import { Draggable } from '@hello-pangea/dnd';
+import { useState } from 'react';
+import { HiOutlineDotsHorizontal } from 'react-icons/hi';
+import styles from './HabitCard.module.css';
+
+const weekdays = [
+  { weekday: 'monday', isDone: Math.random() > 0.5 },
+  { weekday: 'tuesday', isDone: Math.random() > 0.5 },
+  { weekday: 'wednesday', isDone: Math.random() > 0.5 },
+  { weekday: 'thursday', isDone: Math.random() > 0.5 },
+  { weekday: 'friday', isDone: Math.random() > 0.5 },
+  { weekday: 'saturday', isDone: Math.random() > 0.5 },
+  { weekday: 'sunday', isDone: Math.random() > 0.5 },
+];
+
+interface DNDCardProps {
+  index: number;
+}
+
+interface HabitCardProps extends DNDCardProps {
+  id: string;
+  title: string;
+  totalDays: HabitTotalDays;
+  status: HabitStatus;
+  startDate?: Date | undefined;
+}
+
+export function HabitCard({
+  index,
+  id: habitId,
+  title,
+  status,
+  totalDays,
+  startDate,
+}: HabitCardProps) {
+  const passedDays = 8; // TODO: replace it with real data
+
+  const updatePayload: UpdateHabitFormValues = {
+    title,
+    status,
+    totalDays,
+    startDate,
+  };
+
+  const barProgress = (passedDays / totalDays) * 100;
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const actionsRef = useClickOutside<HTMLDivElement>(() => {
+    if (!isMenuOpen) return;
+    setIsMenuOpen(false);
+  });
+
+  const handleMenuClick = () => {
+    // console.log('CLICKED!');
+    setIsMenuOpen(prev => !prev);
+  };
+
+  return (
+    <>
+      <Draggable draggableId={habitId} index={index}>
+        {provided => (
+          <div
+            ref={node => {
+              actionsRef.current = node;
+              if (node) provided.innerRef(node);
+            }}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className={styles.habitCard}
+            style={{ ...provided.draggableProps.style }}
+          >
+            <h3>{title}</h3>
+            <Subtitle>
+              {passedDays} / {totalDays} days
+            </Subtitle>
+            <ProgressBar barHeight="1px" progress={barProgress} />
+
+            <DailyCalendarProgress
+              weekdays={weekdays}
+              showWeekdayLabels={false}
+              dayIndicatorSize="1.3rem"
+              dayIndicatorsGap="0.5rem"
+            />
+
+            <div className={styles.controls}>
+              <Button variant="neutral">Dashboard</Button>
+              <Button variant="icon" onClick={handleMenuClick}>
+                <HiOutlineDotsHorizontal size="2.5rem" />
+              </Button>
+            </div>
+
+            <div
+              className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : ''}`}
+            >
+              <div className={styles.menuContent}>
+                <div className={styles.menuActions}>
+                  <UpdateHabitModalTrigger
+                    onClick={handleMenuClick}
+                    habitId={habitId}
+                    defaultValues={updatePayload}
+                  />
+                  <UpdateHabitStatusButton
+                    onClick={handleMenuClick}
+                    habitId={habitId}
+                    status="paused"
+                  >
+                    Pause
+                  </UpdateHabitStatusButton>
+                  <UpdateHabitStatusButton
+                    onClick={handleMenuClick}
+                    habitId={habitId}
+                    status="built"
+                  >
+                    Move to Built
+                  </UpdateHabitStatusButton>
+                  <DeleteHabitModalTrigger
+                    habitId={habitId}
+                    habitTitle={title}
+                    onClick={handleMenuClick}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </Draggable>
+    </>
+  );
+}
