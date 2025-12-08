dbf5d4c313b6347cabdc0b0044de355d29a3aef6
lebedev-023046 <lebedev.023046@gmail.com>
2025-12-09 01:53:58 +0300

    refactor: update completed option

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -65,37 +65,45 @@ const prisma = new PrismaClient();
 HabitDayStatus;
 
 async function seedHabitLogs(habitId: string, startDate: Date, count: number) {
-  await prisma.habitDayLog.deleteMany({
-    where: {
-      habitId,
-    },
-  });
-
-  const logs = [] as any[];
-
-  for (let i = 0; i < count; i++) {
-    const date = new Date(startDate);
-    date.setDate(startDate.getDate() + i);
-
-    logs.push({
-      date: date.toISOString(),
-      status:
-        Math.random() > 0.3 ? HabitDayStatus.сompleted : HabitDayStatus.missed,
-      habitId,
+  try {
+    await prisma.habitDayLog.deleteMany({
+      where: {
+        habitId,
+      },
     });
+
+    console.log('Deleted previous logs');
+
+    const logs = [] as any[];
+
+    for (let i = 0; i < count; i++) {
+      const date = new Date(startDate);
+      date.setDate(startDate.getDate() + i);
+
+      logs.push({
+        date: date.toISOString(),
+        status:
+          Math.random() > 0.3
+            ? HabitDayStatus.completed
+            : HabitDayStatus.missed,
+        habitId,
+      });
+    }
+
+    await prisma.habitDayLog.createMany({
+      data: logs,
+      skipDuplicates: true,
+    });
+
+    console.log(`Inserted ${logs.length} logs for habit ${habitId}`);
+  } catch (error) {
+    console.log(error);
   }
-
-  await prisma.habitDayLog.createMany({
-    data: logs,
-    skipDuplicates: true,
-  });
-
-  console.log(`Inserted ${logs.length} logs for habit ${habitId}`);
 }
 
 async function main() {
   await seedHabitLogs(
-    '1f81faa7-f9b4-4e4e-92c4-c56ac0ad634e',
+    '4de728e3-d087-4440-bcd7-433769dd27f0',
     new Date('2025-11-23T12:34:56.789Z'),
     10,
   );
