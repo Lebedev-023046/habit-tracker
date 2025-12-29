0c6b776773f9ae086b60f93ec6e37b1a338bab2f
lebedev-023046 <lebedev.023046@gmail.com>
2025-12-29 03:32:08 +0300

    feat: add working production docker + nginx setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,18 @@ const PORT = process.env.PORT ?? 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const server = app.getHttpAdapter().getInstance();
+  server.set('trust proxy', 1);
+
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  app.enableCors([process.env.CLIENT_URL]);
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await app.listen(PORT);
 
