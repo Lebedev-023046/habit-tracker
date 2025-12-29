a07b28e2252ee6fb9e81010b8a70128d85068a1a
lebedev-023046 <lebedev.023046@gmail.com>
2025-12-29 04:44:39 +0300

    fix: use relative /api for production

--- frontend/src/shared/api/instance.ts
+++ frontend/src/shared/api/instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // console.log('VITE_API_URL =', import.meta.env.VITE_API_URL);
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+  baseURL: '/api',
 });
 
 // api.interceptors.request.use(config => {
