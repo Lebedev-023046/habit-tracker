7b8c1d03f1accb7d007192c43942d31940051b0c
lebedev-023046 <lebedev.023046@gmail.com>
2026-01-02 00:54:32 +0300

    refactor: update time service

--- backend/src/common/utils/time/time.service.ts
+++ backend/src/common/utils/time/time.service.ts
@@ -1,17 +1,19 @@
 // src/utils/time/time.service.ts
 
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { startOfDay } from 'date-fns';
 import { toZonedTime } from 'date-fns-tz';
 
 @Injectable()
 export class TimeService {
-  // ПОКА: без пользователей
-  private readonly timezone = 'UTC';
+  private readonly fallbackTimezone = 'UTC';
 
-  today(user: User, date = new Date()): Date {
-    const utcDate = toZonedTime(date, user.timezone ?? this.timezone);
+  now(timezone?: string, date = new Date()): Date {
+    return toZonedTime(date, timezone ?? this.fallbackTimezone);
+  }
+
+  today(timezone: string, date = new Date()): Date {
+    const utcDate = toZonedTime(date, timezone ?? this.fallbackTimezone);
     return startOfDay(utcDate);
   }
 }
